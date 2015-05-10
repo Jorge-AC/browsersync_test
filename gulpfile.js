2c6bd4c5c68092ca1a5d6b874807028f3bd01fb6
Jorge Ardila <jorgeardilac@hotmail.com>
2015-05-09 23:44:52 -0500

    config

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,34 @@
 var gulp = require('gulp');
-var compassImagehelper = require('gulp-compass-imagehelper');
+var compass = require('gulp-compass');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('server-start', ['compass'], function(){
+	browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("sass/**.scss", ['compass']);
+    gulp.watch("*.html").on('change', reload);
+
+});
+ 
+gulp.task('compass', function() {
+  gulp.src('sass/*.scss')
+  	.pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(compass({
+      //config_file: './config.rb',
+      css: 'css',
+      sass: 'sass'
+    }))
+    .pipe(reload({stream: true}));
 });
 
-gulp.task('compass-imagehelper', function () {
-    return gulp.pipe(compassImagehelper({
-
-            images_path: 'images/',
-            css_path: 'css/'
-        }))
-        .pipe(gulp.dest('sass'));
-});
-
-
-
+gulp.task('default', ['server-start']);
