68aa429e813a3b190679d6dd962ed025748c1117
Jorge Ardila <jorgeardilac@hotmail.com>
2015-05-10 20:50:30 -0500

    node-sass added

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,31 @@
 var gulp = require('gulp');
-var compass = require('gulp-compass');
+var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
-gulp.task('server-start', ['compass'], function(){
+gulp.task('server-start', ['sass'], function(){
 	browserSync.init({
         server: {
             baseDir: "./"
         }
     });
 
-    gulp.watch("sass/**.scss", ['compass']);
+    gulp.watch("sass/**.scss", ['sass']);
     gulp.watch("*.html").on('change', reload);
 
 });
  
-gulp.task('compass', function() {
-  gulp.src('sass/*.scss')
-  	.pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(compass({
-      //config_file: './config.rb',
-      css: 'css',
-      sass: 'sass'
-    }))
-    .pipe(reload({stream: true}));
+gulp.task('sass', function () {
+    gulp.src('sass/*.scss')
+      .pipe(sass())
+      .pipe(plumber({
+        errorHandler: function (error) {
+          console.log(error.message);
+          this.emit('end');
+      }}))
+      .pipe(gulp.dest('./css'))
+      .pipe(reload({stream: true}));
 });
 
 gulp.task('default', ['server-start']);
